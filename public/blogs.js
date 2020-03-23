388b51dea9860de301abd14fbd8da87491c60f95
Xander Dean <aaxelld@gmail.com>
2020-03-23 19:15:54 -0400

    added public files and link to public on server

---
+++ public/blogs.js
@@ -0,0 +1,3 @@
+const express= require('express');
+const app = require('express');
+const Blogs = require('../models/blogs.js')
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Blogs = require('./models/blogs.js')
 
+
+app.use('blogs',)
+app.use(express.static('public'))
 
 mongoose.connect(
-    'mongodb://localhost:27017/blog'
+    'mongodb://localhost:27017/blog',
+    {useNewUrlParser:true,useUnifiedTopology:true},
     (req,res)=>{
         console.log('Blog Wars Have Begun')
     }
