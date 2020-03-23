c528720fd33c8c54336c356979badfa181f17903
Xander Dean <aaxelld@gmail.com>
2020-03-23 18:48:59 -0400

    added mongoose to server.js

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+
+mongoose.connect(
+    'mongodb://localhost:27017/' + dbCollection,
+    (req,res)=>{
+        console.log('Blog Wars Have Begun')
+    }
+)
 
 app.listen(3000,(req,res)=>{
     console.log('Blog Wars Online - Port 3000')
