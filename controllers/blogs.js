e2ce3c57c0e4cf00407c9a495247dd472864042c
Clayton O'Neill <claytononeill@Claytons-MacBook-Pro.local>
2020-03-23 19:44:55 -0400

    created the routes for index post delete and update

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -5,13 +5,39 @@ const Blogs = require('../models/blogs.js');
 
 // INDEX ROUTE
 // ========================
+
 router.get('/', (req,res) => {
   Blogs.find({}, (error, foundBlog) => {
-
+    res.json(foundBlog)
   })
 })
 
+// CREATE ROUTE
+// ========================
 
+router.post('/', (req,res) => {
+  Blogs.create(req.body, (err,createdBlog) => {
+    res.json(createdBlog)
+  })
+})
+
+// DELETE ROUTE
+// ========================
+
+router.delete('/:id', (req, res) => {
+  Blogs.findByIdAndRemove(req.params.id, (error, deletedBlog) => {
+    res.json(deletedBlog)
+  })
+})
+
+// UPDATE ROUTE
+// ========================
+
+router.put('/:id', (req, res) => {
+  Blogs.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedBlog) => {
+    res.json(updatedBlog)
+  })
+})
 
 
 module.exports = router;
